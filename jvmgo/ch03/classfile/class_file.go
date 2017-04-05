5cd1020b0cb118670e8282f8716636c3cec7bc48
Peter <Peter@Peters-MacBook-Pro.local>
2017-04-05 23:31:36 +0800

    class reader

---
+++ jvmgo/ch03/classfile/class_file.go
@@ -0,0 +1,17 @@
+package classfile
+
+import "fmt"
+
+type ClassFile struct {
+	maigc        uint32
+	minorVersion uint16
+	majorVersion uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
+	fields       []*MemberInfo
+	methods      []*MemberInfo
+	attributs    []AttributeInfo
+}
