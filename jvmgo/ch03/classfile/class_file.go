01b288cc47fdbbab0146df4a4f3241c1e65eab41
Peter <Peter@Peters-MacBook-Pro.local>
2017-04-05 23:35:49 +0800

    class reader

--- jvmgo/ch03/classfile/class_file.go
+++ jvmgo/ch03/classfile/class_file.go
@@ -3,15 +3,15 @@ package classfile
 import "fmt"
 
 type ClassFile struct {
-	maigc        uint32
-	minorVersion uint16
-	majorVersion uint16
-	constantPool ConstantPool
-	accessFlags  uint16
-	thisClass    uint16
-	superClass   uint16
-	interfaces   []uint16
-	fields       []*MemberInfo
-	methods      []*MemberInfo
-	attributs    []AttributeInfo
+	maigc        uint32          //魔数字CAFE BABE
+	minorVersion uint16          //子版本号
+	majorVersion uint16          //主版本号
+	constantPool ConstantPool    //常量池
+	accessFlags  uint16          //访问控制
+	thisClass    uint16          //本类
+	superClass   uint16          //父类
+	interfaces   []uint16        //接口
+	fields       []*MemberInfo   //字段
+	methods      []*MemberInfo   //方法
+	attributes   []AttributeInfo //
 }
